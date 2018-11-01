16bea046d84984a3bd61fff854a99b6a1d5d50a5
Ayush Gupta <ayush@aicumen.com>
2018-11-01 19:02:49 +0530

    Adding files directory view

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SenderView from './senderView';
 import ReceiverView from './receiverView';
+import FilesView from './filesView';
 import Logo from './kipLogo.png';
-class App extends Component {
 
+class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       tab1:'black',
-      tab2:'black'
+      tab2:'black',
+      tab3: 'black'
     }
   }
 
@@ -31,18 +33,16 @@ class App extends Component {
               <Grid.Row color='black' textAlign='center'>
                 <Grid.Column></Grid.Column>
                 <Grid.Column></Grid.Column>
-                <Grid.Column color={this.state.tab1} style={{borderRight:'1px solid white'}}><Link onClick={() => this.setState({tab1:'grey',tab2:'black'})} to="/sender"><p className={this.state.tab1 === 'grey' ? 'active':'non-active'}> View as Sender</p></Link></Grid.Column>          
-                <Grid.Column color={this.state.tab2}><Link onClick={() => this.setState({tab2:'grey',tab1:'black'})}  to="/receiver"><p className={this.state.tab2 === 'grey' ? 'active':'non-active'}>View as Receiver</p></Link></Grid.Column>
+                <Grid.Column color={this.state.tab1} style={{borderRight:'1px solid white'}}><Link onClick={() => this.setState({tab1:'grey', tab2:'black', tab3:'black'})} to="/sender"><p className={this.state.tab1 === 'grey' ? 'active':'non-active'}>Sender's View</p></Link></Grid.Column>          
+                <Grid.Column color={this.state.tab2} style={{borderRight:'1px solid white'}}><Link onClick={() => this.setState({tab2:'grey', tab3:'black', tab1:'black'})}  to="/receiver"><p className={this.state.tab2 === 'grey' ? 'active':'non-active'}>Receiver's View</p></Link></Grid.Column>
+                <Grid.Column color={this.state.tab3} style={{borderRight:'1px solid white'}}><Link onClick={() => this.setState({tab3:'grey', tab1:'black', tab2:'black'})}  to="/files"><p className={this.state.tab3 === 'grey' ? 'active':'non-active'}>File Directory</p></Link></Grid.Column>
                 <Grid.Column></Grid.Column>
                 <Grid.Column></Grid.Column>
               </Grid.Row>
             </Grid>
-            {/* <Grid style={{width:'300px',margin}} columns={2} padded>
-                <Grid.Column><Link to="/sender">View as Sender</Link></Grid.Column>                
-                <Grid.Column><Link to="/receiver">View as Receiver</Link></Grid.Column>
-            </Grid> */}
             <Route path="/sender" component={SenderView} />
             <Route path="/receiver" component={ReceiverView} />
+            <Route path="/files" component={FilesView} />
           </div>
       </Router>
       </div>
---
+++ src/filesView.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Button, Form, Input, Message, Grid , Image, Card } from 'semantic-ui-react';
+import './App.css';
+import web3 from './ethereum/web3.js';
+import kfs from './ethereum/kfs.js';
+
+class FilesView extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      sender: '',
+      hashMessage:'',
+      receipent:'',
+      realContent:'',
+      source:'',
+      visible:false,
+      items: [],
+      alert:''
+    }
+  }
+
+  
+  componentDidMount() {
+    // const accounts=[];
+    web3.eth.getAccounts().then((accounts, err) => {
+      this.setState({receipent: accounts[0]}); 
+    //   const items = responses.map(response => {
+    //     return {
+    //       header: response.fileName,
+    //       description: response.kfsHash,
+    //       fluid: true,
+    //       style: { overflowWrap: 'break-word' }
+    //     };
+    //   });
+    //   return <Card.Group items={items} />;   
+        kfs.methods.getFilesStruct().call({ from: this.state.receipent }).then((responses, err) => {
+            console.log(responses);
+            const items = responses.map(response => {
+                return {
+                  header: web3.utils.hexToAscii(response.fileName),
+                  description: response.kfsHash,
+                //   meta: response.kfsHash,
+                  fluid: true,
+                  style: { overflowWrap: 'break-word' }
+                };
+              });
+
+            this.setState({ items: items});
+        });
+    });
+  }
+
+  handleDismiss = () => {
+    this.setState({ visible: false });
+  }
+
+  getContent = () => {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <Grid style={{width:'500px'}}>
+              <Grid.Row>
+                <Grid.Column width={16}>
+                <Card.Group items={this.state.items} />
+                </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default FilesView;
