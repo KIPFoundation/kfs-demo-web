649e494d0a4d6d3e63991730020a30b92794582b
Ayush Gupta <ayush@aicumen.com>
2018-11-22 18:58:34 +0530

    Smart contract changes

--- src/ethereum/kfs.js
+++ src/ethereum/kfs.js
@@ -3,7 +3,7 @@ import kfsContract from './build/KFSContract.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(kfsContract.interface),
-   '0x61287cc07c79be9e3b6ffda4755fcbd6babd3368' // -- http://142.93.251.210:8991/
+   '0x9985fcd7020bff29e13c3d56ccedbd65e792ef55' // -- http://142.93.251.210:8991/
 );
 
 export default instance;
